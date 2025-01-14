41075c6782d5c23fed64faceb0352a7d5be27cea
kubilaycicek <kubilaycicek0693@gmail.com>
2025-01-14 23:11:04 +0300

    functions

---
+++ 09_main.py
@@ -0,0 +1,27 @@
+def helloWorld():
+    print("Hello World!")
+    
+def hello(name):
+    print("Hello ",name)
+
+def sayHello(name = "kubilay"):
+    print("Hello ",name)    
+    
+
+def addition(number1,number2):
+    return number1 + number2;
+def divide(number1,number2):
+    return number1 / number2
+
+hello("kubilay")
+helloWorld()
+sayHello()
+sayHello("Sevilay")
+
+result1 = addition(3,4)
+print(result1)
+
+result2 = divide(8,4)
+print(result2)
+
+
